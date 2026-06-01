27bfc6886ebc37630e572b367d5526a5c6ee0414
gpreddy <prasad1annareddy@gmail.com>
2026-06-01 10:41:40 +0530

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape c = new circle { radious = 10 };
+
+            c.Area();
+            c.perimeter();
+            IShape r = new rectangle{length = 10, width = 8};
+            r.Area();
+            r.perimeter();
+
+        }
+    }
+}
---
+++ Interfaces/shapes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IShape
+    {
+         void Area();
+         void perimeter();
+    }
+
+    public class circle:IShape
+    {
+        public double radious { get; set; }
+
+        public void Area()
+        {
+            Console.WriteLine("Area of a circle is :" + (Math.PI * radious * radious));
+        }
+        public void perimeter()
+        {
+            Console.WriteLine("Perimeter of a circle is :" + (2 * Math.PI*radious));
+        }
+    }
+    public class rectangle : IShape
+    {
+        public double length { get; set; }
+        public double width { get; set; }
+        public void Area()
+        {
+            Console.WriteLine("Area of rectangle is :" + (length * width));
+        }
+        public void perimeter()
+        {
+            Console.WriteLine("perimeter of rectangle is :" + 2 * (length + width));
+        }
+    }
+
+}
