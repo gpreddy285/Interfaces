16938803b5ee3ef2e2d430ee3d7f8c0131e6a5a5
gpreddy <prasad1annareddy@gmail.com>
2026-06-01 15:57:13 +0530

    code updated

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -18,17 +18,17 @@ namespace Interfaces
              r.Area();
              r.perimeter();*/
 
-            //expression obj = new expression();
-            //obj.printmessage();
-            //double result = obj.square(17);
-            //Console.WriteLine("The square result is :" + result);
+            /*expression obj = new expression();
+            obj.printmessage();
+            double result = obj.square(17);
+            Console.WriteLine("The square result is :" + result);*/
 
-            Iphonebasic obj1 = new IphoneSe();
+            /*Iphonebasic obj1 = new IphoneSe();
             obj1.calling();
             obj1.receiving();
             obj1.sendmessage();
             obj1.endcall();
-             obj1 = new Iphone17();
+            obj1 = new Iphone17();
             obj1.calling();
             obj1.receiving();
             obj1.sendmessage();
@@ -36,7 +36,17 @@ namespace Interfaces
             Iphoneadvanced obj2 = new Iphone17();
             obj2.faceid();
             obj2.videocall();
-            obj2.faceid();
+            obj2.faceid();*/
+
+            Ipaymentgateway ipg = new paypalgateway();
+            ipg.processpayment();
+            ipg = new upigateway();
+            ipg.processpayment();
+            ipg = new creditcard();
+            ipg.processpayment();
+            ipg = new debitcard();
+            ipg.processpayment();
+
 
 
         }
---
+++ Interfaces/paypalgateway.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface Ipaymentgateway
+    {
+       void processpayment();
+    }
+
+    public class paypalgateway:Ipaymentgateway
+    {
+        public void processpayment() => Console.WriteLine("payment is processed through paypal");
+    }
+    public class upigateway : Ipaymentgateway
+    {
+        public void processpayment() => Console.WriteLine("payment is processed through upi");
+    }
+    public class creditcard : Ipaymentgateway
+    {
+        public void processpayment() => Console.WriteLine("payment is processed through credit card");
+    }
+    public class debitcard : Ipaymentgateway
+    {
+        public void processpayment() => Console.WriteLine("payment is processed through debit card");
+    }
+
+}
